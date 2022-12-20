ef0b757d299be87fbb78821f6cef9149bfc95c03
Gul Akberova <kbrovagul0@gmail.com>
2022-12-20 10:56:17 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useEffect, useState} from 'react';
+
 
 function App() {
+  const [crud, setCrud] = useState([]);
+  const[value,setValue]=useState('')
+
+
+  useEffect(()=>{
+    fetch('https://northwind.vercel.app/api/orders')
+    .then(res=>res.json())
+    .then(data=>{
+      setCrud(data)
+    })
+
+    },[]);
+    
+    const handleDelete=(id)=>{
+      let newCrud=crud.filter(i=>i.id !== id)
+      setCrud(newCrud
+        
+        )
+        
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+    <div className='search_div'>
+      <label>Search</label>
+      <input type={'text'} placeholder='Search' onChange={(e)=>setValue(e.target.value)} />
     </div>
+
+      <table  id="customers">
+        <thead>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>CustomerId</th>
+          <th>OrderDate</th>
+          <th>RequiredDate</th>
+          <th>Delete</th>
+        </tr>
+        </thead>
+        <tbody>
+           {
+              crud.filter(b=>b.shipName.toLowerCase().includes(value)).map((a,key)=>{
+              return(
+                <tr key={a.id}>
+                  <td>{a.id}</td>
+                  <td>{a.shipName}</td>
+                  <td>{a.customerId}</td>
+                  <td>{a.orderDate}</td>
+                  <td>{a.requiredDate}</td>
+                  <td><button onClick={()=>handleDelete(a.id)}><i class="fa-solid fa-trash-can"></i></button></td>
+                </tr>
+              )
+            })
+           }
+        </tbody>
+      </table>
+
+   
+      </>
   );
 }
 
